6fd84056cd9480cfab24d6f60dd38f79cfa539ac
Jordan Thompson <jordanslost@gmail.com>
2015-02-20 14:32:40 -0800

    Added Russian Local
    Added the Russian Language Local

---
+++ messages/ru/base.php
@@ -0,0 +1,15 @@
+<?php
+return array (
+  'You are the owner of this space.' => 'Вы основатель этой Семьи',
+  'Cancel membership' => 'Покинуть',
+  'Request membership' => 'Запросить',
+  'Become member' => 'Вступить',
+  'Accept Invite' => 'Принять',
+  'Decline Invite' => 'Отказать',
+  'Cancel pending membership application' => 'Отменить',
+  'Unfollow' => 'Не наблюдать',
+  'Follow' => 'Наблюдать',
+  'Members:' => 'Членов Семьи:',
+  '<strong>Space</strong> of the Moment' => '<strong>Случайная</strong> Семья',
+  '<i>No description</i>' => '<i>Описания этой Семьи  нет</i>',
+);
