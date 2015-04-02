5c78b26646f148dc2e336ffcfa06bebecc733f23
Jordan Thompson <jordanslost@gmail.com>
2015-04-01 22:28:54 -0700

    Update RandomSpaceSidebarWidget.php
    Updated for 0.11.0. Only public spaces will show.

--- widgets/RandomSpaceSidebarWidget.php
+++ widgets/RandomSpaceSidebarWidget.php
@@ -8,59 +8,60 @@
  * @author Jordan Thompson
  */
 class RandomSpaceSidebarWidget extends HWidget {
-	
-	/**
-	 * Render random space information
-	 *
-	 * @render random space widget
-	 */
-	public function run() {
-	    $css = Yii::app()->assetManager->publish(dirname(__FILE__) . '/../css', true, 0, defined('YII_DEBUG'));
+    
+    /**
+     * Render random space information
+     *
+     * @render random space widget
+     */
+    public function run() {
+        $css = Yii::app()->assetManager->publish(dirname(__FILE__) . '/../css', true, 0, defined('YII_DEBUG'));
         Yii::app()->clientScript->registerCssFile($css . '/randomspace.css');
-		$spaceInfo = $this->getRandomSpace();
-		if ( is_object($spaceInfo[0]) && is_array($spaceInfo[1]) ) {
-			$this->render ( 'RandomSpacePanel', array (
-				'css' => $css,
-				'space' => $spaceInfo[0],
-				'members' => $spaceInfo[1]
-			) );
-		// Something went wrong... try again
-		} else {
-			$this->run();
-		}
+        $spaceInfo = $this->getRandomSpace();
+        if ( is_object($spaceInfo[0]) && is_array($spaceInfo[1]) ) {
+            $this->render ( 'RandomSpacePanel', array (
+                'css' => $css,
+                'space' => $spaceInfo[0],
+                'members' => $spaceInfo[1]
+            ) );
+        // Something went wrong... try again
+        } else {
+            $this->run();
+        }
 
-	}
-	
-	
-	/**
-	 * Get a random space model from DB
-	 *
-	 * @return array of space and membership information from Space Model.
-	 */
-	private function getRandomSpace() {
-	
-		$max = Space::model()->count();
-		$randId = rand(0,$max);
-		$space = Space::model()->find(array('offset'=>$randId));
-		if ( is_object($space) ) {
-			$members = array();
-			$membership = Yii::app()->db->createCommand()
-				->select('user_id')
-				->from('space_membership')
-				->where('space_id=:id', array(':id'=>$space->id))
-				->queryAll();
-			foreach ( $membership as $member ) {
-				$members[] = User::model()->findByPk($member['user_id']);
-			}
-			return array($space, $members);
-		// Something went wrong.. try again
-		} else {
-			$this->getRandomSpace();
-		}
-		
-	}
-	
-	
+    }
+    
+    
+    /**
+     * Get a random space model from DB
+     *
+     * @return array of space and membership information from Space Model.
+     */
+    private function getRandomSpace() {
+    
+        $max = Space::model()->count();
+        $randId = rand(0,$max);
+        $space = Space::model()->find(array('offset'=>$randId));
+        if ($space->attributes['visibility'] == 2) {
+            if ( is_object($space) ) {
+                    $members = array();
+                $membership = Yii::app()->db->createCommand()
+                        ->select('user_id')
+                    ->from('space_membership')
+                    ->where('space_id=:id', array(':id'=>$space->id))
+                    ->queryAll();
+                foreach ( $membership as $member ) {
+                    $members[] = User::model()->findByPk($member['user_id']);
+                }
+                return array($space, $members);
+            // Something went wrong.. try again
+            } else {
+                $this->getRandomSpace();
+            }
+        } else {
+            $this->getRandomSpace();
+        }
+    }
+    
+    
 }
-
-?>
